7787c6a4f65f0738f2ad47138d51eee780019066
Ethan Barnacoat <ethan.barnacoat@gmail.com>
2026-05-15 17:41:50 +1000

    hopefully fixing the api security issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import ClientWrapper from './ClientWrapper';
+import ClientWrapper from './ClientWrapper'; // <-- Fixed to ./
 
 export const metadata: Metadata = {
   title: 'PrintLayers',
